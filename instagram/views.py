502631247bbcb2f6edde20c1f60b15cbe7cc71ed
saudahabib <saudababs00@gmail.com>
2019-05-19 01:36:34 +0300

    connect main project to instagram application

---
+++ instagram/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+'''welcome view to process landing page'''
+def welcome(request):
+    return HttpResponse("Welcome fella")
