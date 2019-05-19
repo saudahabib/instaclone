6f173ef22f7a347e60720f628146553720704a18
saudahabib <saudababs00@gmail.com>
2019-05-19 10:51:20 +0300

    made profile and post model for the posts

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to = 'articles/', blank = True)
+    bio=models.CharField(max_length =50)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/', blank = True)
+    image_name = models.CharField(max_length =50)
+    image_caption = models.CharField(max_length =50)
+    profile = models.ForeignKey(Profile)
+    likes = models.CharField(max_length =50)
+    comments = models.CharField(max_length =50)
