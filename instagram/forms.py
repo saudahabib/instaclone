2dddfa2e045684cdaa8406084a5681e1b90fedb2
saudahabib <saudababs00@gmail.com>
2019-05-20 15:49:18 +0300

    create a new form for creating a new profile

--- instagram/forms.py
+++ instagram/forms.py
@@ -5,3 +5,7 @@ class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['likes', 'comments', 'profile']
+
+class NewProfileForm(form.ModelForm):
+    class Meta:
+        model = Profile
